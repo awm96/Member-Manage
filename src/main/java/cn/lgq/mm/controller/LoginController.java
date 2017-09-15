b3cafe860e21e044306e3a6273236add1f056c7d
awm96 <awm96@163.com>
2017-09-15 20:52:40 +0800

    test

--- src/main/java/cn/lgq/mm/controller/LoginController.java
+++ src/main/java/cn/lgq/mm/controller/LoginController.java
@@ -1,5 +1,6 @@
 package cn.lgq.mm.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by Ligq on 2017/9/15.
  */
+@Slf4j
 @Controller
 @RequestMapping("/login")
 public class LoginController extends AbstractController {
@@ -15,6 +17,7 @@ public class LoginController extends AbstractController {
     /** 跳转至member登陆页面 */
     @RequestMapping(value = "/member", method = RequestMethod.GET)
     public String forwardToMember() {
+        log.info("asdf");
         return "login_member";
     }
 
